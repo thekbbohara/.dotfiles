98d65a0f365098da5873911686a9a3500e4d5ef9
kbzcraft <ytkbzcraft@gmail.com>
2024-07-30 20:48:18 +0545

    show month and date also on tooltip

--- .config/waybar/sctipts/clock.sh
+++ .config/waybar/sctipts/clock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clock=echo "$(date +%I:%M) $(date +%p)"
-echo -e "{\"text\":\"$(date +%I:%M) $(date +%p)\" ,\"tooltip\": \"$(date +%A)\"}"
+echo -e "{\"text\":\"$(date +%I:%M) $(date +%p)\" ,\"tooltip\": \"$(date +%b) $(date +%d) $(date +%a)\"}"
