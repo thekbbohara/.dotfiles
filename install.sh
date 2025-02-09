74d02a45f5c9705dabf2e6cac5e67d7faa6c32e1
thekbbohara <kb@arch.localdomain>
2025-02-09 11:16:27 +0000

    upd

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
-stow
-wl-clipboard
-clipse
-waybar
-swaync
-hypridle 
-pypr
-exa
-neovim
-google-chrome
-alacritty
-nautilus
-rofi
-pyprland
-fzf
-sudo pacman -S noto-fonts noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd ttf-firacode-nerd --needed
+
+#!/bin/bash
+
+# Update system and install base dependencies
+echo "Updating system and installing base packages..."
+sudo pacman -Syu --noconfirm
+
+# Install packages from pacman repositories
+echo "Installing packages from pacman repositories..."
+sudo pacman -S --needed stow zsh zoxide wl-clipboard clipse waybar swaync hypridle hyprlock pypr exa neovim alacritty nautilus rofi fzf grim slurp noto-fonts noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd --noconfirm
+
+# Install missing font (ttf-firacode-nerd) from AUR using yay
+echo "Installing ttf-firacode-nerd from AUR..."
+yay -S --needed ttf-firacode-nerd --noconfirm
+
+# Install packages from AUR using yay (if not in pacman)
+echo "Installing AUR packages using yay..."
+yay -S --needed google-chrome pyprland clipse --noconfirm
+
+# Install zinit (Zsh plugin manager)
+# echo "Installing zinit..."
+# sh -c "$(curl -fsSL https://git.io/zinit-install)"
+
+# Make zsh the default shell
+# echo "Setting zsh as the default shell..."
+# which zsh
+# chsh -s "$(which zsh)"
+
+# Print installation complete message
+# echo "All packages and configurations have been installed and set up!"
+
+# Restart shell to apply changes
+# exec zsh
+
