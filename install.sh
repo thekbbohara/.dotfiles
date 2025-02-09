6b7b5a2a46c01c54d750a297ee9f3a372cd79547
Kb Bohara <ytkbzcraft@gmail.com>
2025-02-09 11:36:52 +0545

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+stow
+wl-clipboard
+clipse
+waybar
+swaync
+hypridle 
+pypr
+exa
+neovim
+google-chrome
+alacritty
+nautilus
+rofi
+pyprland
+fzf
+sudo pacman -S noto-fonts noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd ttf-firacode-nerd --needed
