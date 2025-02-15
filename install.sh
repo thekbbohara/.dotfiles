f089698088d5cde7ff39a77923c033815ade24c6
thekbbohara <thekbbohara@gmail.com>
2025-02-15 13:37:10 +0545

    djsdkajf

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ sudo pacman -Syu --noconfirm
 
 # Install packages from pacman repositories
 echo "Installing packages from pacman repositories..."
-sudo pacman -S --needed stow zsh zoxide wl-clipboard clipse waybar swaync hypridle hyprlock pypr exa neovim alacritty nautilus rofi fzf grim slurp noto-fonts noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd xdg-desktop-portal-hyprland brightnessctl qt6ct qt5ct  --noconfirm 
+sudo pacman -S --needed stow zsh zoxide wl-clipboard clipse waybar swaync hypridle hyprlock pypr exa neovim alacritty nautilus rofi fzf grim slurp noto-fonts noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono-nerd xdg-desktop-portal-hyprland brightnessctl qt6ct qt5ct unzip  --noconfirm 
 # Install missing font (ttf-firacode-nerd) from AUR using yay
 echo "Installing ttf-firacode-nerd from AUR..."
 yay -S --needed ttf-firacode-nerd  --noconfirm
 
 # Install packages from AUR using yay (if not in pacman)
 echo "Installing AUR packages using yay..."
-yay -S --needed google-chrome pyprland clipse swww loupe --noconfirm
+yay -S --needed google-chrome pyprland clipse swww loupe mpv  --noconfirm
 
 # Install zinit (Zsh plugin manager)
 # echo "Installing zinit..."
